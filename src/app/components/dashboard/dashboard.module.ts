f29c6e5ae2d7fae59b0b07d4edbb5888f7f6a320
Dida02 <rudzani123@gmail.com>
2021-02-08 12:54:11 +0200

    Dashboard Sales Application

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardRoutingModule } from './dashboard-routing.module';
-//import { DashboardComponent } from './dashboard.component';
-//import { SalesApplicationFormModule } from '../sales-application-form/sales-application-form.module';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms'; // Reactive form services
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [],
   imports: [
-    //DashboardRoutingModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
-    //SalesApplicationFormModule
   ],
   bootstrap: [],
   exports: [],
